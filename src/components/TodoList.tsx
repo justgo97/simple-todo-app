3be8df200ccf5fdef2ba6051a94aa06c1434a6e7
hamdi2050 <hamdi.mar20@gmail.com>
2023-07-23 18:27:24 +0100

    Split TodoItem component to a file

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,5 @@
-import { getAllTodos, useAppDispatch, useAppSelector } from "../store";
-import { todosActions } from "../store/todosReducer";
-import { ITodo } from "../types";
+import { getAllTodos, useAppSelector } from "../store";
+import TodoItem from "./TodoItem";
 
 const TodoList = () => {
   const todoList = useAppSelector(getAllTodos());
@@ -23,62 +22,6 @@ const TodoList = () => {
         </p>
       )}
     </>
-  );
-};
-
-interface TodoItemProps {
-  todo: ITodo;
-}
-
-const TodoItem = ({ todo }: TodoItemProps) => {
-  const dispatch = useAppDispatch();
-
-  const onClickDelete = () => {
-    //
-    dispatch(todosActions.deleteTodo(todo.id));
-  };
-
-  const onClickToggle = () => {
-    //
-    dispatch(todosActions.toggleTodo(todo.id));
-  };
-
-  return (
-    <div className="todo-list-item">
-      <div className="todo-list-item-main">
-        <h3 className="todo-list-item-main-title">
-          <span
-            className={`todo-list-item-main-title-status ${
-              todo.completed ? "todo-list-item-main-title-status-completed" : ""
-            }`}
-          ></span>
-          <span>{todo.title}</span>
-        </h3>
-        <p
-          className={`todo-list-item-main-description ${
-            todo.completed ? "todo-list-item-main-description-completed" : ""
-          }`}
-        >
-          {todo.description}
-        </p>
-      </div>
-      <div className="todo-list-item-footer">
-        <button
-          className={`todo-list-item-footer-toggle ${
-            todo.completed ? "todo-list-item-footer-toggle-completed" : ""
-          }`}
-          onClick={onClickToggle}
-        >
-          Complete
-        </button>
-        <button
-          className="todo-list-item-footer-delete"
-          onClick={onClickDelete}
-        >
-          Delete
-        </button>
-      </div>
-    </div>
   );
 };
 
