67b2eb28a63f5d910dba2c670bda7eedfe411fb0
hamdi2050 <hamdi.mar20@gmail.com>
2023-07-25 09:38:30 +0100

    Fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/simple-todo-app/",
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
